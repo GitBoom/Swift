fe82b2a22bdaef4bd415c42ef93929e4e5f2457e
wuhao <a20131105803@mac21.local>
2016-03-21 19:58:12 +0800

    03.21

---
+++ 20131105803/20131105803/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20131105803
+//
+//  Created by 20131105803 on 16/3/21.
+//  Copyright © 2016年 20131105803. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
