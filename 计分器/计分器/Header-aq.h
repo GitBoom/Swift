63996e67f5ad2b18e907064315c210984b406fe8
wuhao <a20131105803@mac21.local>
2016-04-25 21:27:02 +0800

    04.25
    添加数据库；
    UI改进；
    没有了；

---
+++ 计分器/计分器/Header-aq.h
@@ -0,0 +1,11 @@
+//
+//  Header-aq.h
+//  计分器
+//
+//  Created by 20131105803 on 16/4/25.
+//  Copyright © 2016年 20131105803. All rights reserved.
+//
+
+
+#import "sqlite3.h"
+#import <time.h>
--- 计分器/计分器/计分器/ViewController.swift
+++ 计分器/计分器/计分器/ViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var db:SQLiteDB!
     var ShowNumber = 0
     var ShowNumber2 = 0
+    var remainTime:Double!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        //如果有数据则加载
+        initUser()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     @IBOutlet weak var LeftTextField: UITextField!
     @IBOutlet weak var RightTextField: UITextField!
+    @IBOutlet weak var Team1: UITextField!
+    @IBOutlet weak var Team2: UITextField!
     @IBAction func Left3(sender: AnyObject) {
         Touch(3)
     }
@@ -43,6 +53,33 @@ class ViewController: UIViewController {
     }
     @IBAction func Cancel(sender: AnyObject) {
     }
+    @IBOutlet weak var CountDown: UIDatePicker!
+    @IBAction func Begin(sender: AnyObject) {
+        
+    }
+    @IBAction func Save(sender: AnyObject) {
+        save()
+    }
+    func initUser() {
+        let data = db.query("select * from t_user")
+        if data.count > 0 {
+            //获取最后一行数据显示
+            let user = data[data.count - 1]
+            LeftTextField.text = user["uname"] as? String
+            RightTextField.text = user["mobile"] as? String
+        }
+    }
+    func save() {
+        let uname = self.LeftTextField.text!
+        let mobile = self.RightTextField.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        print("sql: \(sql)")
+        //通过封装的方法执行sql
+        let result = db.execute(sql)
+        print(result)
+    }
+    
     
     func Touch(Number:Int){
         ShowNumber=ShowNumber + Number
